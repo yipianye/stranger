f9cc7d0af1053767cef56b8d66207ffb37c8e342
ZiBaiCai <59802054+ZiBaiCai@users.noreply.github.com>
2021-02-18 18:09:06 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ $(function() {
       box_shadow: '#FA7199',
       icon: 'img/workgroup.png',
     }, {
-      name: '腾讯兔小巢',
+      name: '兔小巢',
       link: 'https://support.qq.com/product/309330',
       box_shadow: '#F2584A',
       icon: 'img/qq.jpg',
